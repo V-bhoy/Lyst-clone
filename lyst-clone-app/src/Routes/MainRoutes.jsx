60b5e5513289c629b0cf003aff001fbe345b2deb
V-bhoy <vaishubhoyar004@gmail.com>
2022-05-05 23:54:19 +0530

    Added homepage

--- lyst-clone-app/src/Routes/MainRoutes.jsx
+++ lyst-clone-app/src/Routes/MainRoutes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Home from "../pages/Home";
 import Products from "../pages/Products";
 
 const MainRoutes = () => {
@@ -8,6 +9,7 @@ const MainRoutes = () => {
     <>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Home/>} />
         <Route path="/products/:id" element={<Products />} />
       </Routes>
     </>
