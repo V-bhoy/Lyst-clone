59e6a1f96e9755c0a58e3fa0e723fa29d3b4e7ad
V-bhoy <vaishubhoyar004@gmail.com>
2022-05-05 23:38:12 +0530

    updated

--- lyst-clone-app/src/App.js
+++ lyst-clone-app/src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import MainRoutes from "./Routes/MainRoutes";
 
 function App() {
   return (
     <div className="App">
-       <MainRoutes />
+       
     </div>
   );
 }
