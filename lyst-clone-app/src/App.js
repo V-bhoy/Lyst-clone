6292ac35791357411478a83053c24764ec86eb8e
haresh2349 <hareshcsolanki07@gmail.com>
2022-05-06 09:46:35 +0530

    Update

--- lyst-clone-app/src/App.js
+++ lyst-clone-app/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Footer from "./components/Footer";
-import Shipping from "./components/Shipping";
+import Shipping from "./pages/Shipping";
 
 function App() {
   return (
     <div className="App">
-      <Shipping />
-      {/* <Footer /> */}
+      {/* <Shipping /> */}
+      <Footer />
     </div>
   );
 }
