4a8a64c7ee3b3e6159ca0c6febd870733a38ce00
V-bhoy <vaishubhoyar004@gmail.com>
2022-05-06 10:02:25 +0530

    updated with footer

--- lyst-clone-app/src/Routes/MainRoutes.jsx
+++ lyst-clone-app/src/Routes/MainRoutes.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import Home from "../pages/Home";
 import Products from "../pages/Products";
@@ -12,6 +13,7 @@ const MainRoutes = () => {
         <Route path="/" element={<Home/>} />
         <Route path="/products/:id" element={<Products />} />
       </Routes>
+      <Footer />
     </>
   );
 };
--- lyst-clone-app/src/components/Footer.jsx
+++ lyst-clone-app/src/components/Footer.jsx
@@ -1,9 +1,352 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import {
+  FooterContainer,
+  InnerContainer,
+  LeftContainer,
+  Trustpilot,
+  StarRatings,
+  HalfStarBox,
+  ScoreReviewsBox,
+  SocialMediaHandler,
+  StoreContainer,
+  BottomText,
+  MiddleContainer,
+  RightContainer,
+  PopupContainer,
+  PopupChild,
+  InnerChild,
+  QrBox,
+  Mobile,
+  MobileDisplay,
+} from "./FooterStyled";
+import StarIcon from "@mui/icons-material/Star";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import CloseIcon from "@mui/icons-material/Close";
+import AddIcon from "@mui/icons-material/Add";
 const Footer = () => {
+  const [popup, setPopup] = useState(false);
+  const [accordian1, setAccordian1] = useState(false);
+  const [accordian2, setAccordian2] = useState(false);
   return (
-    <div>Footer</div>
-  )
-}
+    <FooterContainer>
+      <InnerContainer>
+        <LeftContainer>
+          <Trustpilot>
+            <StarIcon />
+            <h2>Trustpilot</h2>
+          </Trustpilot>
+          <StarRatings>
+            <div>
+              <StarIcon />
+            </div>
+            <div>
+              <StarIcon />
+            </div>
+            <div>
+              <StarIcon />
+            </div>
+            <div>
+              <StarIcon />
+            </div>
+            <HalfStarBox>
+              <StarIcon />
+            </HalfStarBox>
+          </StarRatings>
+          <ScoreReviewsBox>
+            <p>
+              TrustScore <b>4.5</b>
+            </p>
+            <p>
+              <b>2668 </b> reviews
+            </p>
+          </ScoreReviewsBox>
 
-export default Footer
+          <SocialMediaHandler>
+            <FacebookIcon />
+            <InstagramIcon />
+          </SocialMediaHandler>
+          <StoreContainer>
+            <div
+              onClick={() => {
+                setPopup(true);
+              }}
+              style={{ width: "130px" }}
+            >
+              <img
+                style={{ width: "100%" }}
+                src="https://www.svgrepo.com/show/303128/download-on-the-app-store-apple-logo.svg"
+                alt="AppleStore"
+              />
+            </div>
+            <div
+              onClick={() => {
+                setPopup(true);
+              }}
+              style={{ width: "175px" }}
+            >
+              <img
+                style={{ width: "100%" }}
+                src="https://www.logo.wine/a/logo/Google_Play/Google_Play-Badge-Logo.wine.svg"
+                alt="PlayStore"
+              />
+            </div>
+          </StoreContainer>
+          <BottomText>
+            <p>
+              <a href="/">
+                Learn about the Lyst app for iPhone, iPad and Android.
+              </a>
+            </p>
+            <p>© 2022 Lyst</p>
+          </BottomText>
+        </LeftContainer>
+        <MiddleContainer>
+          <div>
+            <h2>INTERNATIONAL</h2>
+          </div>
+          <ul>
+            <li>
+              <a href="/">Lyst - AU</a>
+            </li>
+            <li>
+              <a href="/">Lyst - CA</a>
+            </li>
+            <li>
+              <a href="/">Lyst - UK</a>
+            </li>
+            <li>
+              <a href="/">Lyst - Österreich</a>
+            </li>
+            <li>
+              <a href="/">Lyst - Schweiz</a>
+            </li>
+            <li>
+              <a href="/">Lyst - Deutschland</a>
+            </li>
+            <li>
+              <a href="/">Lyst - España</a>
+            </li>
+            <li>
+              <a href="/">Lyst - France</a>
+            </li>
+            <li>
+              <a href="/">Lyst - Italia</a>
+            </li>
+            <li>
+              <a href="/">Lyst - 日本</a>
+            </li>
+            <li>
+              <a href="/">Lyst - België</a>
+            </li>
+            <li>
+              <a href="/">Lyst - Nederland</a>
+            </li>
+          </ul>
+        </MiddleContainer>
+        <MobileDisplay>
+          <div>
+            <h2>INTERNATIONAL</h2>
+            <AddIcon
+              onClick={() => {
+                setAccordian1(!accordian1);
+              }}
+            />
+          </div>
+          {accordian1 && (
+            <ul>
+              <li>
+                <a href="/">Lyst - AU</a>
+              </li>
+              <li>
+                <a href="/">Lyst - CA</a>
+              </li>
+              <li>
+                <a href="/">Lyst - UK</a>
+              </li>
+              <li>
+                <a href="/">Lyst - Österreich</a>
+              </li>
+              <li>
+                <a href="/">Lyst - Schweiz</a>
+              </li>
+              <li>
+                <a href="/">Lyst - Deutschland</a>
+              </li>
+              <li>
+                <a href="/">Lyst - España</a>
+              </li>
+              <li>
+                <a href="/">Lyst - France</a>
+              </li>
+              <li>
+                <a href="/">Lyst - Italia</a>
+              </li>
+              <li>
+                <a href="/">Lyst - 日本</a>
+              </li>
+              <li>
+                <a href="/">Lyst - België</a>
+              </li>
+              <li>
+                <a href="/">Lyst - Nederland</a>
+              </li>
+            </ul>
+          )}
+        </MobileDisplay>
+        <RightContainer>
+          <div>
+            <h2>HELP AND INFO</h2>
+          </div>
+          <ul>
+            <li>
+              <a href="">Help center</a>
+            </li>
+            <li>
+              <a href="">About us</a>
+            </li>
+            <li>
+              <a href="">Shipping policy</a>
+            </li>
+            <li>
+              <a href="">Returns policy</a>
+            </li>
+            <li>
+              <a href="">Payments</a>
+            </li>
+            <li>
+              <a href="">Refund policy</a>
+            </li>
+            <li>
+              <a href="">Developers</a>
+            </li>
+            <li>
+              <a href="">Careers</a>
+            </li>
+            <li>
+              <a href="">Contact</a>
+            </li>
+            <li>
+              <a href="">Terms & conditions</a>
+            </li>
+            <li>
+              <a href="">Privacy & cookie policy</a>
+            </li>
+            <li>
+              <a href="">Intellectual property</a>
+            </li>
+            <li>
+              <a href="">Categories</a>
+            </li>
+            <li>
+              <a href="">Become a partner</a>
+            </li>
+            <li>
+              <a href="">Lyst Insights</a>
+            </li>
+            <li>
+              <a href="">Lyst News</a>
+            </li>
+          </ul>
+        </RightContainer>
+        <MobileDisplay>
+          <div>
+            <h2>HELP AND INFO</h2>
+            <AddIcon
+              onClick={() => {
+                setAccordian2(!accordian2);
+              }}
+            />
+          </div>
+          {accordian2 && (
+            <ul>
+              <li>
+                <a href="">Help center</a>
+              </li>
+              <li>
+                <a href="">About us</a>
+              </li>
+              <li>
+                <a href="">Shipping policy</a>
+              </li>
+              <li>
+                <a href="">Returns policy</a>
+              </li>
+              <li>
+                <a href="">Payments</a>
+              </li>
+              <li>
+                <a href="">Refund policy</a>
+              </li>
+              <li>
+                <a href="">Developers</a>
+              </li>
+              <li>
+                <a href="">Careers</a>
+              </li>
+              <li>
+                <a href="">Contact</a>
+              </li>
+              <li>
+                <a href="">Terms & conditions</a>
+              </li>
+              <li>
+                <a href="">Privacy & cookie policy</a>
+              </li>
+              <li>
+                <a href="">Intellectual property</a>
+              </li>
+              <li>
+                <a href="">Categories</a>
+              </li>
+              <li>
+                <a href="">Become a partner</a>
+              </li>
+              <li>
+                <a href="">Lyst Insights</a>
+              </li>
+              <li>
+                <a href="">Lyst News</a>
+              </li>
+            </ul>
+          )}
+        </MobileDisplay>
+      </InnerContainer>
+      {popup && (
+        <PopupContainer>
+          <PopupChild>
+            <InnerChild>
+              <QrBox>
+                <button
+                  onClick={() => {
+                    setPopup(false);
+                  }}
+                >
+                  <CloseIcon />
+                </button>
+                <div>
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/128/241/241521.png"
+                    alt="qrCode"
+                  />
+                </div>
+              </QrBox>
+              <h1>Scan the code on your phone</h1>
+              <h3>
+                Open the camera on your phone to scan and download the Lyst app
+                for free
+              </h3>
+              <p>Available on iOS and Android</p>
+              <Mobile
+                src="https://static.lystit.com/static/n/web/react/components/qrcode-overlay/images/get-app-phone.ba25ca91295f8c2fa372763d098933cd.png"
+                alt=""
+              />
+            </InnerChild>
+          </PopupChild>
+        </PopupContainer>
+      )}
+    </FooterContainer>
+  );
+};
+
+export default Footer;
