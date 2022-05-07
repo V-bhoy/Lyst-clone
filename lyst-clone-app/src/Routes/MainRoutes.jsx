c355d84f7ac713fc76c416765bfd5e8f83e800c4
niteshrawat0401 <niteshrawat0401@gmail.com>
2022-05-07 18:59:43 +0530

    add data

--- lyst-clone-app/src/Routes/MainRoutes.jsx
+++ lyst-clone-app/src/Routes/MainRoutes.jsx
@@ -9,6 +9,10 @@ import LoggedIn from "../pages/logged-in/LoggedIn";
 import Cart from "../pages/Cart";
 import Home from "../pages/Home";
 import Products from "../pages/Products";
+import { Helpcenter } from "../pages/Help Center/Helpcenter";
+import { ShoppingonLyst } from "../pages/Help Center/ShoppingonLyst";
+import { Contact } from "../pages/Help Center/Contact";
+import { About_us } from "../pages/Help Center/About_us";
 
 const MainRoutes = () => {
   return (
@@ -19,6 +23,10 @@ const MainRoutes = () => {
         <Route path="/cart" element={<Cart/>} />
         <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
+          <Route path='/help' element={<Helpcenter/>}/>
+      <Route path='/ShoppingonLyst' element={<ShoppingonLyst/>}/>
+      <Route path='/contact-us' element={<Contact/>}/>
+      <Route path='/aboutus' element={<About_us/>}/>
           <Route path="/logged-in" element={<LoggedIn />} />
         <Route path="/products/:id" element={<Products />} />
         <Route path="*" element={<Error />} />
